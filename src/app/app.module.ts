6b6fb0ea67c445167758c52b37fed8640ea201d9
nadusmilli <nadusmilli@Admins-MacBook-Pro-4.local>
2019-02-12 12:11:48 -0800

    Adding navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [NavbarComponent, AppComponent],
     imports: [HttpClientModule, BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent]
