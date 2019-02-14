5ab4975e4bdaf3e5ab589c54099d8dc4780ee546
nadusmilli <nadusmilli@Admins-MacBook-Pro-4.local>
2019-02-14 11:56:57 -0800

    Making imports more convinient

---
+++ src/app/services/index.ts
@@ -0,0 +1,5 @@
+export { ProductsService } from "./products/products.service";
+export { Product } from "./products/product";
+export { CartService } from "./carts/cart.service";
+export { Cart } from "./carts/cart";
+export { Item } from "./carts/Item";
